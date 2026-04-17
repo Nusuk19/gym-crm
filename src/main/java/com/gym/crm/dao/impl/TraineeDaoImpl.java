1e1bdd1daf77b31efa206b7bc9ec0c585bcb698c
Artem Kraynyk <kraynyk_a2015@ukr.net>
2026-04-17 10:43:56 +0300

    GIA-30 Implement dao layer

---
+++ src/main/java/com/gym/crm/dao/impl/TraineeDaoImpl.java
@@ -0,0 +1,57 @@
+package com.gym.crm.dao.impl;
+
+import com.gym.crm.dao.TraineeDao;
+import com.gym.crm.exception.EntityNotFoundException;
+import com.gym.crm.model.Trainee;
+import com.gym.crm.storage.InMemoryStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class TraineeDaoImpl implements TraineeDao {
+
+    private InMemoryStorage inMemoryStorage;
+
+    @Autowired
+    public void setInMemoryStorage(InMemoryStorage inMemoryStorage) {
+        this.inMemoryStorage = inMemoryStorage;
+    }
+
+    private Map<Long, Trainee> storage() {
+        return inMemoryStorage.getTraineeStorage();
+    }
+
+    @Override
+    public Trainee save(Trainee trainee) {
+        storage().put(trainee.getUserId(), trainee);
+        return trainee;
+    }
+
+    @Override
+    public Trainee update(Trainee trainee) {
+        if (!storage().containsKey(trainee.getUserId())) {
+            throw new EntityNotFoundException("Trainee not found: " + trainee.getUserId());
+        }
+        storage().put(trainee.getUserId(), trainee);
+        return trainee;
+    }
+
+    @Override
+    public void delete(Long userId) {
+        storage().remove(userId);
+    }
+
+    @Override
+    public Optional<Trainee> findTraineeById(Long id) {
+        return Optional.ofNullable(storage().get(id));
+    }
+
+    @Override
+    public List<Trainee> findAllTrainees() {
+        return List.copyOf(storage().values());
+    }
+}
---
+++ src/main/java/com/gym/crm/dao/impl/TrainerDaoImpl.java
@@ -0,0 +1,52 @@
+package com.gym.crm.dao.impl;
+
+import com.gym.crm.dao.TrainerDao;
+import com.gym.crm.exception.EntityNotFoundException;
+import com.gym.crm.model.Trainer;
+import com.gym.crm.storage.InMemoryStorage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class TrainerDaoImpl implements TrainerDao {
+
+    private InMemoryStorage inMemoryStorage;
+
+    @Autowired
+    public void setInMemoryStorage(InMemoryStorage inMemoryStorage) {
+        this.inMemoryStorage = inMemoryStorage;
+    }
+
+    private Map<Long, Trainer> storage() {
+        return inMemoryStorage.getTrainerStorage();
+    }
+
+    @Override
+    public Trainer save(Trainer trainer) {
+        storage().put(trainer.getUserId(), trainer);
+        return trainer;
+    }
+
+    @Override
+    public Trainer update(Trainer trainer) {
+        if (!storage().containsKey(trainer.getUserId())) {
+            throw new EntityNotFoundException("Trainer not found: " + trainer.getUserId());
+        }
+        storage().put(trainer.getUserId(), trainer);
+        return trainer;
+    }
+
+    @Override
+    public Optional<Trainer> findTrainerById(Long id) {
+        return Optional.ofNullable(storage().get(id));
+    }
+
+    @Override
+    public List<Trainer> findAllTrainers() {
+        return List.copyOf(storage().values());
+    }
+}
