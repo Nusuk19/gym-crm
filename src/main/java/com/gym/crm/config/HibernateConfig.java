ac26bb05ea4196fab2bfa68283a5ed351eb13284
Artem Krainyk <kraynyk_a2015@ukr.net>
2026-04-25 16:43:42 +0300

    GIA-61 Configure Hibernate
    * GIA-61 Configure Hibernate
    
    * GIA-61 Configure Hibernate - Canceling logging in HibernateConfig and fixing pom.xml
    
    * GIA-61 Configure Hibernate - delete empty line

---
+++ src/main/java/com/gym/crm/config/HibernateConfig.java
@@ -0,0 +1,45 @@
+package com.gym.crm.config;
+
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Properties;
+
+@Configuration
+public class HibernateConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(HibernateConfig.class);
+
+    @Value("${db.driver}")
+    private String driver;
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Bean(destroyMethod = "close")
+    public SessionFactory sessionFactory() {
+        Properties properties = new Properties();
+        properties.put("hibernate.connection.driver_class", driver);
+        properties.put("hibernate.connection.url", url);
+        properties.put("hibernate.connection.username", username);
+        properties.put("hibernate.connection.password", password);
+        properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.hbm2ddl.auto", "none");
+
+        return  new org.hibernate.cfg.Configuration()
+                .addProperties(properties)
+                .buildSessionFactory();
+    }
+}
