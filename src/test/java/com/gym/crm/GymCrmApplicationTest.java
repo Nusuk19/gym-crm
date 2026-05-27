f5fc731f44b35bf1562a48361419c21c5185a3c4
Artem Kraynyk <kraynyk_a2015@ukr.net>
2026-05-27 18:52:42 +0300

    GIA-172 Migrate application to Spring Boot

--- src/test/java/com/gym/crm/GymCrmApplicationTest.java
+++ src/test/java/com/gym/crm/GymCrmApplicationTest.java
@@ -1,21 +1,22 @@
 package com.gym.crm;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class GymCrmApplicationTest {
 
+    @Autowired
+    private ApplicationContext context;
     @Test
-    void contextLoads() {
-    }
-
-    @Test
-    void main_shouldNotThrow() {
-        assertDoesNotThrow(() -> GymCrmApplication.main(new String[]{}));
+    void context_shouldLoadSuccessfully() {
+        assertThat(context).isNotNull();
     }
 }
