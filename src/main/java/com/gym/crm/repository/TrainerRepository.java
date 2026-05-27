c33440d006200745063943132e7c0d1c31f22707
Artem Kraynyk <kraynyk_a2015@ukr.net>
2026-05-27 23:17:24 +0300

    GIA-172 Migrate application to Spring Boot - Add anotation @Reposotiry for repo

--- src/main/java/com/gym/crm/repository/TrainerRepository.java
+++ src/main/java/com/gym/crm/repository/TrainerRepository.java
@@ -4,10 +4,12 @@ import com.gym.crm.model.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface TrainerRepository extends JpaRepository<Trainer, Long> {
 
     @Query("""
--- src/main/java/com/gym/crm/repository/TrainingRepository.java
+++ src/main/java/com/gym/crm/repository/TrainingRepository.java
@@ -4,9 +4,11 @@ import com.gym.crm.model.Training;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TrainingRepository extends JpaRepository<Training, Long>, JpaSpecificationExecutor<Training> {
 
     @Query("""
